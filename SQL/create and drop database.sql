ddc64a36fe93fd5f5bcff878787fec857322e490
Matan Toledano <75612523+matantoled@users.noreply.github.com>
2024-08-16 17:50:07 +0300

    first leeson on SQL - create and drop database

---
+++ SQL/create and drop database.sql
@@ -0,0 +1,18 @@
+-- create a database called myDB:
+create database myDB;
+
+/* 
+-- to drop a database we can do:
+drop database myDB;		
+*/
+
+
+
+/* 
+-- in order to prevent deleting our database we can do:
+alter database myDB read only = 1;
+
+
+-- to disable read only mode:
+alter database myDB read only = 0;
+*/
